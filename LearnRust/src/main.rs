f07a288761f5aee9633d99de0b4eaec25019c126
karl14000 <81111824+karl14000@users.noreply.github.com>
2022-03-06 09:22:36 +0100

    Section 7/22

--- LearnRust/src/main.rs
+++ LearnRust/src/main.rs
@@ -39,21 +39,99 @@ s.push_str("jour"); // s = "Bonjour"
 s.push("!");
 dbg!(s); // s = "Bonjour!"
 */
-fn main() {
-    let s = "Voici mon premier message";
+// fn main() {
+//     let s = "Voici mon premier message";
 
-    println!("{}", s.contains("premier"));
-    println!("{}", s.replace("nom","ton"));
-    println!("{}", s.starts_with("Voici"));
-    println!("{}", s.ends_with("message"));
-    println!("{}", s.to_uppercase());
+//     println!("{}", s.contains("premier"));
+//     println!("{}", s.replace("nom","ton"));
+//     println!("{}", s.starts_with("Voici"));
+//     println!("{}", s.ends_with("message"));
+//     println!("{}", s.to_uppercase());
 
-    println!("{:?}", s.find("mon")); // Some(6)
+//     println!("{:?}", s.find("mon")); // Some(6)
 
-    match s.find("mon") {
-        Some(n) => println!("position = {}",n),
-        None => println!("non présent"),
-    }
-}
+//     match s.find("mon") {
+//         Some(n) => println!("position = {}",n),
+//         None => println!("non présent"),
+//     }
+// }
 // position 6
 
+
+// fn add(a: &String, b: &String) -> String { // on peut aussi remplacer String par str
+
+//     format!(" {} {}", a, b)
+// }
+
+// fn main() {
+//     let h = String::from("Hello");
+//     let w = String::from("World");
+
+//     println!(" h + w = {}", add(&h, &w));
+//     print!("h = {}", h)
+//  }
+
+// fn push(a: &mut String, b:&str) {
+//     a.push_str(" ");
+//     a.push_str(b);
+// }
+// fn main(){
+//     let mut h = String::from("hello");
+//     let w = String::from("World");
+
+//     push(&mut h, &w);
+//     println!("h = {}", h);
+// }
+   
+/*
+Valeur : T
+Object : T
+Ref immuable : &T
+Ref modifiable &mutT
+*/
+
+// Tuple 
+
+// fn main() {
+//     let t = ("Paris", 19);
+//     println!("{} {} °C", t.0, t.1);
+// }
+// fn main() {
+//     let (add, mul) = add_mul(3, 4);
+//     println!("Add : {}", add);
+//     println!("Mul : {}", mul);
+// }
+
+// fn add_mul(a: i32, b: i32) -> (i32, i32) {
+//     (a + b, a * b)
+
+// }
+// let s ("Paris",); // tuple d'un element doit comporter un virgule
+// mot clé nom définition
+// struct Position(i32, i32);
+
+// fn main(){
+//     let p = Position(1, 1);
+//     let x = p.0;
+//     let y = p.1;
+//     let Position(x, y ) = p;
+//     print!("({}, {})", x, y);
+// }
+
+// struct Position { // mot clé  nom
+//     //definition
+//     x: i32, // attribut, type
+//     y: i32, 
+// }
+
+// fn main() {
+//     let p = Position{ x: 1,y: 1 };
+//     //récupération champ par champ
+//     let x = p.x;
+//     let y = p.y;
+//     // recupération par pattern matching
+//     let Position {x: x, y: y} = p;
+//     let Position { x, y } = p;
+//     //affichage
+//     println!("x={}, y={}",x , y); //
+// }

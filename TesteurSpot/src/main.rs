c53583386e4ace4526fc10a83632c551faecddd4
karl14000 <81111824+karl14000@users.noreply.github.com>
2022-04-06 07:51:37 +0200

    Test add

--- TesteurSpot/src/main.rs
+++ TesteurSpot/src/main.rs
@@ -1,6 +1,11 @@
-// Test Resp API
-
+// Test
 
 fn main() {
+    let mut entier64: u64 = 5;
+    let flottant32 :f32 = 6.3;
+
+    entier64 = 12;
+
+    println!("L'entier est égal à : {}", entier64)
 
 }
